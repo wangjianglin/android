27aea3ba9606b612dc47b177f8c263a218fd7f6d
wangjianglin <184041008@qq.com>
2017-06-15 16:53:22 +0800

    1、支付 https 通信 2、添加自定义导航栏功能

---
+++ core/src/main/java/lin/core/ResToolsFragment.java
@@ -0,0 +1,50 @@
+package lin.core;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import lin.core.annotation.OptionsMenu;
+
+
+/**
+ * Created by lin on 30/12/2016.
+ */
+
+//处理注解
+public class ResFragment extends AbsFragment {
+
+
+    private int mResId = 0;
+
+    public ResFragment(){
+    }
+
+    protected ResFragment(int resId){
+        this.mResId = resId;
+    }
+    public LayoutInflater getLayoutInflater(Bundle savedInstanceState) {
+        return LayoutInflaterFactory.setFactory2(super.getLayoutInflater(savedInstanceState));
+    }
+
+    private View mView;
+    @Nullable
+    @Override
+    final public View onCreateViewInternal(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if(mResId != 0){
+            mView = inflater.inflate(mResId,container,false);
+        }else {
+            mView = inflater.inflate(Views.layoutId(this), container, false);
+        }
+        return mView;
+    }
+
+    @Nullable
+    @Override
+    public View getView() {
+        return mView;
+    }
+
+}
---
+++ demo/src/main/java/lin/demo/http/HttpActivity.java
@@ -0,0 +1,47 @@
+package lin.demo.form;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import lin.core.LayoutInflaterFactory;
+import lin.core.ptr.PtrView;
+import lin.demo.R;
+import lin.demo.databinding.ActivityFormBinding;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_ptr_default);
+        super.onCreate(savedInstanceState);
+//        ActivityFormBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_form);
+//        setContentView(R.layout.activity_form);
+        ActivityFormBinding binding = ActivityFormBinding.inflate(LayoutInflaterFactory.from(this));
+
+        FormData data = new FormData();
+        data.setSegueCls(FormView.class);
+        binding.setData(data);
+
+        setContentView(binding.getRoot());
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
